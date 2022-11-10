17c5ec89a0b701f7da20e4f2f1cab6a4dbc9bb85
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:46:09 +0600

    review form added

--- src/Pages/Review/ReviewForm.js
+++ src/Pages/Review/ReviewForm.js
@@ -9,7 +9,53 @@ const ReviewForm = ({ service }) => {
 
     return (
         <div>
+            <div className=" mx-auto flex flex-col max-w-xl p-8 shadow-sm rounded-xl lg:p-12 dark:bg-gray-900 dark:text-gray-100">
+                <form onSubmit={handleReview}>
+                    <div className="flex flex-col items-center w-full">
+                        <h2 className="text-3xl font-semibold text-center">Your opinion matters!</h2>
+                        <div className='w-full mb-2'>
+                            <div className="mb-2 block">
+                                <Label
+                                    htmlFor="rating"
+                                    value="Your Rating (example: 4.2 or 5)"
+                                />
+                            </div>
+                            <TextInput
+                                className='w-full'
+                                id="rating"
+                                type="text"
+                                placeholder="Rating"
+                                required={true}
+                                name='rating'
+                            />
+                        </div>
+                        <div className='w-full mb-2'>
+                            <div className="mb-2 block">
+                                <Label
+                                    htmlFor="email1"
+                                    value="Your email"
+                                />
+                            </div>
+                            <TextInput
+                                id="email1"
+                                type="email"
+                                placeholder="name@email.com"
+                                required={true}
+                                name='email'
+                                defaultValue={user?.email}
+                                readOnly
+                            />
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <textarea rows="3" placeholder="Message..." name='message' className="p-4 rounded-md resize-none dark:text-gray-100 dark:bg-gray-900" required></textarea>
+                            <Button className='mt-3' type="submit">
+                                Submit
+                            </Button>
+                        </div>
+                    </div>
+                </form>
 
+            </div>
         </div>
     );
 };
