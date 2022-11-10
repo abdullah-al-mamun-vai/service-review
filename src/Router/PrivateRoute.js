4bb5503661e5c724531d0d66ac63c9bc95ffb8ba
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:54:05 +0600

    private route added

--- src/Router/PrivateRoute.js
+++ src/Router/PrivateRoute.js
@@ -3,7 +3,25 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { UserContext } from '../Pages/Auth/AuthContext';
 import { Bars } from 'react-loader-spinner'
 const PrivateRoute = ({ children }) => {
-
+    const location = useLocation();
+    const { user, loading } = useContext(UserContext)
+    if (!user) {
+        return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>
+    }
+    if (loading) {
+        return <div className="mx-auto w-10">
+            <Bars
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="bars-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </div>
+    }
+    return children
 };
 
 export default PrivateRoute;
