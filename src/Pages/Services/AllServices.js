3edc9e7e2ffcbd8087fbf2653cd5a5c5f7350143
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:49:37 +0600

    all services page added

--- src/Pages/Services/AllServices.js
+++ src/Pages/Services/AllServices.js
@@ -4,13 +4,19 @@ import UseTitle from '../../TitleHook/TitleHook';
 import Service from './Service';
 
 const AllServices = () => {
-   
+    const services = useLoaderData();
+
     // title set 
     UseTitle('Services')
 
     return (
         <div className='py-8'>
-           
+            <div className=' grid grid-cols-3 gap-3'>
+                {
+                    services.map(service => <Service service={service} key={service._id}></Service>)
+                }
+            </div>
+
 
         </div >
     );
