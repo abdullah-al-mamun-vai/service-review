606b87f35ed8698b355dd07e7e159e27535c9fd6
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:39:19 +0600

    sign up form added

--- src/Pages/Auth/Sign.js
+++ src/Pages/Auth/Sign.js
@@ -11,7 +11,81 @@ const Sign = () => {
 
     return (
         <div className="lg:w-2/5 mx-auto border py-7 px-3">
-
+            <form onSubmit={handleRegis} className="flex flex-col gap-4 ">
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="name"
+                            value="Name"
+                        />
+                    </div>
+                    <TextInput
+                        id="name"
+                        type="text"
+                        placeholder="Name"
+                        required={true}
+                        name='name'
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="photo"
+                            value="Photo URL"
+                        />
+                    </div>
+                    <TextInput
+                        id="photo"
+                        type="text"
+                        placeholder="Photo URL"
+                        required={true}
+                        name='photo'
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="email1"
+                            value="Your email"
+                        />
+                    </div>
+                    <TextInput
+                        id="email1"
+                        type="email"
+                        placeholder="name@email.com"
+                        required={true}
+                        name='email'
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="password1"
+                            value="Your password"
+                        />
+                    </div>
+                    <TextInput
+                        id="password1"
+                        type="password"
+                        required={true}
+                        name='password'
+                    />
+                </div>
+                <div className="flex items-center gap-2">
+                    <Checkbox id="remember" />
+                    <Label htmlFor="remember">
+                        Remember me
+                    </Label>
+                </div>
+                <Button type="submit">
+                    Submit
+                </Button>
+            </form>
+            <div>
+                <Button onClick={handleFace} className='w-full  mt-2'>
+                    Log in with <FaFacebook></FaFacebook>
+                </Button>
+            </div>
         </div>
     );
 };
