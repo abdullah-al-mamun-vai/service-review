9302bd6e1710babea7f9021d4ddcd5df1256b967
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:42:44 +0600

    home services card and section added

--- src/Pages/Home/HomeCard.js
+++ src/Pages/Home/HomeCard.js
@@ -14,7 +14,44 @@ const HomeCard = () => {
     }, [])
     return (
         <div className='py-8'>
-         
+            <div className=' grid lg:grid-cols-3 gap-3'>
+                {/* home services section area  */}
+                {
+                    homeServices.map(service => <div className="" key={service._id}>
+                        <Card>
+                            <div className='relative'>
+                                {/* services thumnail  */}
+                                <PhotoProvider>
+                                    <PhotoView key={service._id} src={service.img}>
+                                        <img src={service.img} className='h-44 w-full' alt="" />
+                                    </PhotoView>
+                                </PhotoProvider>
+                                <p className='flex items-center absolute bottom-0 right-0 bg-lime-600 text-white p-3'>{service.rating}<FaStar className='text-amber-500 ml-1' /></p>
+                            </div>
+                            {/* services title and description  */}
+                            <div className='h-44'>
+                                <h5 className="h-28 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {service.service_name}
+                                </h5>
+                                <p className="font-normal text-gray-700 dark:text-gray-400">
+                                    {service?.description.length > 100 ? service.description.slice(0, 100) + '...' : service.description}
+                                </p>
+                            </div>
+                            <div className='flex justify-between items-center'>
+                                <p className='font-semibold capitalize'>Service Fee: {service.price}</p>
+                                <Button color="success">
+                                    <Link to={`services/${service._id}`} className="flex items-center capitalize"> view details <FaArrowRight className='ml-1' /></Link>
+                                </Button>
+                            </div>
+                        </Card>
+                    </div>)
+                }
+                {/* home services area end  */}
+            </div>
+
+            <Button className='mx-auto mt-7' color="success">
+                <Link to={'/services'} className="flex items-center"> See All <FaArrowRight className='ml-1' /></Link>
+            </Button>
         </div >
     );
 };
