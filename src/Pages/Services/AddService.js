7e4ab9896b22a480d8e6273f98f0ed86a53873eb
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:48:23 +0600

    service added design added

--- src/Pages/Services/AddService.js
+++ src/Pages/Services/AddService.js
@@ -8,11 +8,93 @@ const AddService = () => {
     UseTitle('Add Service')
 
     // handle service add 
-   
+
     return (
         <div>
 
-           
+            <div className=" w-4/5  lg:w-2/5 mx-auto border py-7 px-3">
+                <form onSubmit={handleService} className="flex flex-col gap-4 ">
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="name"
+                                value="Service Name"
+                            />
+                        </div>
+                        <TextInput
+                            id="name"
+                            type="text"
+                            placeholder="Service Name"
+                            required={true}
+                            name='name'
+                        />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="photo"
+                                value="Service Photo URL"
+                            />
+                        </div>
+                        <TextInput
+                            id="photo"
+                            type="text"
+                            placeholder="Service Photo URL"
+                            required={true}
+                            name='photo'
+                        />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="price"
+                                value="Service Fee"
+                            />
+                        </div>
+                        <TextInput
+                            id="price"
+                            type="text"
+                            placeholder="Service Fee"
+                            required={true}
+                            name='price'
+                        />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="rating"
+                                value="Rating"
+                            />
+                        </div>
+                        <TextInput
+                            id="rating"
+                            type="text"
+                            placeholder="Rating"
+                            required={true}
+                            name='rating'
+                        />
+                    </div>
+                    <div id="textarea">
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="comment"
+                                value="Description"
+                            />
+                        </div>
+                        <Textarea
+                            id="comment"
+                            placeholder="Description..."
+                            required={true}
+                            rows={4}
+                            name='description'
+                        />
+                    </div>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </form>
+
+            </div>
         </div>
     );
 };
