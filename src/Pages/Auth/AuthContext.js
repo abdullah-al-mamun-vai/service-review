f34fe1c99db4c91e3da4c7a6c237e6269a16c078
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:35:58 +0600

    handle log in added auth

--- src/Pages/Auth/AuthContext.js
+++ src/Pages/Auth/AuthContext.js
@@ -4,10 +4,39 @@ import app from '../../firebase/firebase.init';
 export const UserContext = createContext();
 const auth = getAuth(app);
 const AuthContext = ({ children }) => {
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true);
+    console.log(user)
+    const handleSign = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    // handele Log in function 
+    const handleLog = (email, pass) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, pass);
+    }
 
+    // handle sign out 
+    const handleOut = () => {
+        signOut(auth).then(() => {
+
+        })
+    }
+    // user context value 
+    const info = {
+        handleSign,
+        handleLog,
+        handleFb,
+        user,
+        handleOut,
+        loading
+    }
     return (
         <div>
-
+            <UserContext.Provider value={info}>
+                {children}
+            </UserContext.Provider>
 
         </div>
     );
