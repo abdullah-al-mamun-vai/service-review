77e78624b78570868f619063cee569a6a62a31d2
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-10 13:45:09 +0600

    my review table design added

--- src/Pages/Review/MyReview.js
+++ src/Pages/Review/MyReview.js
@@ -11,7 +11,54 @@ const MyReview = () => {
 
     return (
         <div className='container mx-auto'>
+            <Table hoverable={true}>
+                <Table.Head>
+                    <Table.HeadCell className="!p-4">
 
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Product name
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Rating
+                    </Table.HeadCell>
+
+                    <Table.HeadCell>
+                        Price
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        <span className="sr-only">
+                            Edit
+                        </span>
+                    </Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                    {
+                        myReview.map(review => <Table.Row key={review._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="!p-4">
+                                <button onClick={() => handleDlt(review._id)}> <FaTrash></FaTrash></button>
+                            </Table.Cell>
+                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                {review.service_name}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {review.rating}
+                            </Table.Cell>
+
+                            <Table.Cell>
+
+                                {review.price}
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Button>
+                                    <Link to={`/edit-reviews/${review._id}`}><FaEdit /></Link>
+                                </Button>
+                            </Table.Cell>
+                        </Table.Row>)
+                    }
+
+                </Table.Body>
+            </Table>
         </div>
     );
 };
